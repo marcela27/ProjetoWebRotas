2ce888a6378b3a979e9a238964d289fe8158b785
marcela27 <marcela.almeida17@etec.sp.gov.br>
2023-02-24 17:29:06 -0300

    first commit

---
+++ meuPrimeiroProjetoLaravel-main/resources/views/empresa.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <title>Laravel Empresas</title>
+
+    </head>
+    <body class="antialiased">
+      Empresa
+      <a class="btn btn-danger" href="produtos.php">Produtos</button>
+      <br>
+       <a class="btn btn-danger" href="welcome.blade.php">Home</button>
+       <br>
+       <a class="btn btn-danger" href="contato.php">Contato</button>
+    </body>
+</html>
